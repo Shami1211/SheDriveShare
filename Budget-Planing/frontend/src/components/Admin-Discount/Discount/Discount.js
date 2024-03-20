98e6c27df6a28fba5833c1f1fb77b9ec1b5c8ff9
Shami1211 <it21203558@my.sliit.lk>
2024-03-20 19:39:30 +0530

    added image

--- Budget-Planing/frontend/src/components/Admin-Discount/Discount/Discount.js
+++ Budget-Planing/frontend/src/components/Admin-Discount/Discount/Discount.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Discount = (props) => {
-  const { _id, name, company, price, discount, total, start, end } = props.drive; // Change props.discount to props.drive
+  const { _id, image, name, company, price, discount, total, start, end } = props.drive;
 
   return (
     <div>
+      <img src={image} alt={name} onError={() => console.log('Error loading image:', image)} />
       <h2>{name}</h2>
+      
       <h4>Company: {company}</h4>
       <h4>Price: ${price}</h4>
       <h4>Discount: {discount}%</h4>
